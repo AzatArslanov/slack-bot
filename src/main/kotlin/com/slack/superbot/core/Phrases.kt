2026af03bb90e08559a0ef83df0b6a4a8a352c72
Azat Arslanov <a.r.arslanov@gmail.com>
2018-02-06 13:18:51 +0300

    update Phrases - remove mention one devops from call devops phrase

--- src/main/kotlin/com/slack/superbot/core/Phrases.kt
+++ src/main/kotlin/com/slack/superbot/core/Phrases.kt
@@ -2,7 +2,7 @@ package com.slack.superbot.core
 
 object Phrases {
     const val CALL_DEVOPS = "призываю девопсов"
-    const val CALL_DEVOPS_REPLAY = "<@U8TK4K39A>, <@U8K82BHQV> и <@U8QQCPZNE> тут без вас никак"
+    const val CALL_DEVOPS_REPLAY = "<@U8K82BHQV> и <@U8QQCPZNE> тут без вас никак"
     const val IT_IS_OKAY = "не против"
     const val IT_IS_OKAY_REPLAY = "лично я не против"
     const val ALL_IN_FAVOR = "все за"
